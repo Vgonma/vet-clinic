3c15d070a7e1a5937ba4e8397fbef9d78dfb8255
Vgonma <victorgmassimi.microverse@gmail.com>
2023-02-23 08:23:40 -0600

    added join tables specialization and visits

--- schema.sql
+++ schema.sql
@@ -30,3 +30,21 @@ ALTER TABLE animals ADD species_id integer;
 ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
 ALTER TABLE animals ADD owner_id integer;
 ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name varchar(100),
+    age integer,
+    date_of_graduation date
+);
+
+CREATE TABLE specializations (
+    vet_id integer,
+    species_id integer
+);
+
+CREATE TABLE visits (
+    animal_id integer,
+    vet_id integer,
+    visit_date date
+);
