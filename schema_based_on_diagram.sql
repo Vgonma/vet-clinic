09c9c81a2aa7d8abb0951032de0d9e1c2e708376
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-28 21:43:28 +0600

    Add medical_histories table.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS patients (
   name TEXT,
   date_of_birth DATE
 );
+
+CREATE TABLE IF NOT EXISTS medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at DATE,
+  patient_id INT REFERENCES patients(id),
+  status TEXT
+);
