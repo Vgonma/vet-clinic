8b749f1b89862deb01d5cd8c43a6ff6ea84cc897
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-28 21:43:47 +0600

    Add invoices table.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,3 +10,11 @@ CREATE TABLE IF NOT EXISTS medical_histories (
   patient_id INT REFERENCES patients(id),
   status TEXT
 );
+
+CREATE TABLE IF NOT EXISTS invoices (
+  id SERIAL PRIMARY KEY,
+  total_ammount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history__id INT REFERENCES medical_histories(id)
+);
