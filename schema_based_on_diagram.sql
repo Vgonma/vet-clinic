fd0ac8adac4aeb9fbbc61bfaec646bbe6c11f377
Victor Gonzalez <victorgmassimi.microverse@gmail.com>
2023-02-28 13:51:23 -0600

    Add indexes to all foreign keys.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,6 +11,9 @@ CREATE TABLE IF NOT EXISTS medical_histories (
   status TEXT
 );
 
+CREATE INDEX idx_patient_id
+ON medical_histories(patient_id ASC);
+
 CREATE TABLE IF NOT EXISTS invoices (
   id SERIAL PRIMARY KEY,
   total_ammount DECIMAL,
@@ -18,6 +21,9 @@ CREATE TABLE IF NOT EXISTS invoices (
   payed_at TIMESTAMP,
   medical_history__id INT REFERENCES medical_histories(id)
 );
+
+CREATE INDEX idx_medical_histories__id
+ON invoices(medical_histories__id ASC);
 
 CREATE TABLE IF NOT EXISTS treatments (
   id SERIAL PRIMARY KEY,
@@ -29,6 +35,12 @@ CREATE TABLE IF NOT EXISTS histories_treatments (
   history_id INT REFERENCES medical_histories(id),
   treatment_id INT REFERENCES treatments(id)
 );
+
+CREATE INDEX idx_history_id 
+ON histories_treatments(history_id ASC);
+
+CREATE INDEX idx_treatment_id
+ON histories_treatments(treatment_id ASC);
 
 CREATE TABLE IF NOT EXISTS invoice_items (
   id SERIAL PRIMARY KEY,
